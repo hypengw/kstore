dd4ba9a27ec2dc0b582cc6e337bd43e7596db72e
hypengw <hypengwip@gmail.com>
2025-08-10 13:39:30 +0000

    from json

--- include/kstore/qt/meta_utils.hpp
+++ include/kstore/qt/meta_utils.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
 #include <QtCore/QJsonDocument>
+#include <QtCore/QMetaType>
 #include <QtCore/QVariant>
 
 namespace kstore
 {
 
 auto qvariant_to_josn(const QVariant&) -> QJsonValue;
-auto qvariant_from_josn(const QJsonValue&) -> QVariant;
+auto qvariant_from_josn(const QMetaType&, const QJsonValue&) -> QVariant;
+
+template<typename T>
+auto qvariant_from_josn(const QJsonValue& val) -> std::optional<T> {
+    auto out = qvariant_from_josn(QMetaType::fromType<T>(), val);
+    if (out.isValid() && out.metaType() == QMetaType::fromType<T>()) {
+        return out.template value<T>();
+    }
+    return std::nullopt;
+}
 
 } // namespace kstore
--- src/qt/meta_utils.cpp
+++ src/qt/meta_utils.cpp
@@ -22,6 +22,25 @@ auto qgadget_to_json(const QVariant& variant) -> QJsonObject {
     }
     return obj;
 }
+
+auto qgadget_from_json(const QMetaType& type, const QJsonObject& obj) -> QVariant {
+    QVariant gadget(type, nullptr);
+    auto     meta = type.metaObject();
+    for (int i = 0; i < meta->propertyCount(); ++i) {
+        auto prop = meta->property(i);
+        if (prop.metaType().flags() & QMetaType::IsPointer) {
+            // ignore
+        } else if (prop.isWritable() && prop.isStored()) {
+            auto name = QString::fromUtf8(prop.name());
+            auto v    = obj.value(name);
+            if (! v.isUndefined()) {
+                QVariant propVar = kstore::qvariant_from_josn(prop.metaType(), v);
+                prop.writeOnGadget(gadget.data(), propVar);
+            }
+        }
+    }
+    return gadget;
+} // namespace
 } // namespace
 
 auto kstore::qvariant_to_josn(const QVariant& variant) -> QJsonValue {
@@ -45,13 +64,6 @@ auto kstore::qvariant_to_josn(const QVariant& variant) -> QJsonValue {
         return arr;
     } else if (auto p = get_if<QStringList>(&variant); p) {
         return QJsonValue::fromVariant(variant);
-    } else if (variant.canConvert<QSequentialIterable>()) {
-        auto arr  = QJsonArray();
-        auto view = variant.value<QSequentialIterable>();
-        for (const auto& item : view) {
-            arr.append(qvariant_to_josn(item));
-        }
-        return arr;
     } else if (variant.canConvert<QAssociativeIterable>()) {
         auto obj  = QJsonObject();
         auto view = variant.value<QAssociativeIterable>();
@@ -77,7 +89,88 @@ auto kstore::qvariant_to_josn(const QVariant& variant) -> QJsonValue {
         else if (type.flags() & QMetaType::IsGadget) {
             return qgadget_to_json(variant);
         }
+        // try QString
+        else if (variant.canConvert<QString>()) {
+            return variant.toString();
+        }
+        // try sequential iterable
+        else if (variant.canConvert<QSequentialIterable>()) {
+            auto arr  = QJsonArray();
+            auto view = variant.value<QSequentialIterable>();
+            for (const auto& item : view) {
+                arr.append(qvariant_to_josn(item));
+            }
+            return arr;
+        }
     }
     return QJsonValue::fromVariant(variant);
 }
-auto kstore::qvariant_from_josn(const QJsonValue&) -> QVariant { return {}; }
+
+auto kstore::qvariant_from_josn(const QMetaType& type, const QJsonValue& value) -> QVariant {
+    if (QMetaType::hasRegisteredConverterFunction(QMetaType::fromType<QJsonValue>(), type)) {
+        QVariant out(type, nullptr);
+        QMetaType::convert(QMetaType::fromType<QJsonValue>(), &value, type, out.data());
+        return out;
+    }
+    if (value.isObject()) {
+        if (type.id() == QMetaType::QVariantHash) {
+            QVariantHash map;
+            QJsonObject  obj = value.toObject();
+            for (auto it = obj.begin(); it != obj.end(); ++it) {
+                map.insert(it.key(),
+                           qvariant_from_josn(QMetaType::fromType<QVariant>(), it.value()));
+            }
+            return map;
+        } else if (type.id() == QMetaType::QVariantMap) {
+            QVariantMap map;
+            QJsonObject obj = value.toObject();
+            for (auto it = obj.begin(); it != obj.end(); ++it) {
+                map.insert(it.key(),
+                           qvariant_from_josn(QMetaType::fromType<QVariant>(), it.value()));
+            }
+            return map;
+        }
+        if (QMetaType::hasRegisteredConverterFunction(QMetaType::fromType<QJsonObject>(), type)) {
+            QVariant out(type, nullptr);
+            auto     obj = value.toObject();
+            QMetaType::convert(QMetaType::fromType<QJsonObject>(), &obj, type, out.data());
+            return out;
+        }
+        if (type.flags() & QMetaType::IsGadget) {
+            auto obj = value.toObject();
+            return qgadget_from_json(type, obj);
+        }
+    } else if (value.isArray()) {
+        if (type.id() == QMetaType::QVariantList) {
+            QVariantList list;
+            QJsonArray   arr = value.toArray();
+            for (const auto& v : arr) {
+                list.append(qvariant_from_josn(QMetaType::fromType<QVariant>(), v));
+            }
+            return list;
+        }
+        if (type.id() == QMetaType::QStringList) {
+            QStringList list;
+            QJsonArray  arr = value.toArray();
+            for (const auto& v : arr) {
+                list.append(v.toString());
+            }
+            return list;
+        }
+        if (QMetaType::hasRegisteredConverterFunction(QMetaType::fromType<QJsonArray>(), type)) {
+            QVariant out(type, nullptr);
+            auto     arr = value.toArray();
+            QMetaType::convert(QMetaType::fromType<QJsonArray>(), &arr, type, out.data());
+            return out;
+        }
+    }
+
+    auto variant = value.toVariant();
+    if (variant.metaType().id() == type.id()) {
+        return variant;
+    } else if (variant.convert(type)) {
+        return variant;
+    }
+
+    return QVariant {};
+}
